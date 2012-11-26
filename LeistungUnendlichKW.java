39c0e6750d92a37dfacb11efdd0bea767fef62cf
Morka <Nief@mx.at>
2012-11-26 20:39:43 +0100

    -

---
+++ LeistungUnendlichKW.java
@@ -0,0 +1,42 @@
+/**
+* @author Matthias Gusenbauer, Wolfgang Hofer, Alexander Neff
+*/
+
+public class LeistungUnendlichKW extends Leistung{
+	/*public String toString(){
+		return "1KW Leistung";
+	}
+	
+	public void vonHilfskraftBenutzt(B_Hilfskraft android){
+		android.setLeistung(this);
+	}
+	
+	public void vonGesellschafterBenutzt(B_Gesellschafter android) {
+		android.setLeistung(this);
+	}
+	
+	public void vonBauarbeiterBenutzt(S_Bauarbeiter android) {
+		
+	}
+	
+	public void vonServicetechnikerBenutzt(S_ServiceTechniker android) {
+		
+	}
+	
+	public void vonTransportarbeiterBenutzt(S_Transportarbeiter android) {
+		
+	}
+	
+	public void vonObjektbewacherBenutzt(G_Objektbewacher android) {
+		
+	}
+	
+	public void vonLeibwaechterBenutzt(G_Leibwaechter android) {
+		
+	}
+	
+	
+	public void vonKaempferBenutzt(G_Kaempfer android) {
+		
+	}*/
+}
