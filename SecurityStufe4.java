4ad5c722646cc433ac57cd2f7c35a209288d173f
Morka <Nief@mx.at>
2012-11-26 19:49:40 +0100

    Added Security

---
+++ SecurityStufe4.java
@@ -0,0 +1,34 @@
+/**
+ * @author Matthias Gusenbauer, Wolfgang Hofer, Alexander Neff
+ */
+ 
+public class SecurityStufe4 extends Security{
+	public String toString(){
+		return "Security Stufe 4";
+	}
+	
+	public void vonHilfskraftSoftwareBenutzt(HilfskraftSoftware software){
+	
+	}
+	public void vonGesellschafterSoftwareBenutzt(GesellschafterSoftware software){
+	
+	}
+	public void vonBauarbeiterSoftwareBenutzt(BauarbeiterSoftware software){
+	
+	}
+	public void vonServiceTechnikerSoftwareBenutzt(ServiceTechnikerSoftware software){
+	
+	}
+	public void vonTransportarbeiterSoftwareBenutzt(TransportarbeiterSoftware software){
+	
+	}
+	public void vonObjektbewacherSoftwareBenutzt(ObjektbewacherSoftware software){
+	
+	}
+	public void vonLeibwaechterSoftwareBenutzt(LeibwaechterSoftware software){
+	
+	}
+	public void vonKaempferSoftwareBenutzt(KaempferSoftware software){
+	
+	}
+}
