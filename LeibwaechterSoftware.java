e635b179e57eb3220008aecebb3aee710b1ed91d
Morka <Nief@mx.at>
2012-11-27 22:10:43 +0100

    LeibwaechterSoftware finished

--- LeibwaechterSoftware.java
+++ LeibwaechterSoftware.java
@@ -8,35 +8,43 @@ public class LeibwaechterSoftware extends Software {
 	}
 	
 	public void vonHilfskraftBenutzt(B_Hilfskraft android) {
+		android.setSoftware(null);
 		
 	}
 	
 	public void vonGesellschafterBenutzt(B_Gesellschafter android) {
+		android.setSoftware(null);
 		
 	}
 	
 	public void vonBauarbeiterBenutzt(S_Bauarbeiter android) {
+		android.setSoftware(null);
 		
 	}
 	
 	public void vonServicetechnikerBenutzt(S_ServiceTechniker android) {
+		android.setSoftware(null);
 		
 	}
 	
 	public void vonTransportarbeiterBenutzt(S_Transportarbeiter android) {
+		android.setSoftware(null);
 		
 	}
 	
 	public void vonObjektbewacherBenutzt(G_Objektbewacher android) {
-		
+		android.setSoftware(null);
+
 	}
 	
 	public void vonLeibwaechterBenutzt(G_Leibwaechter android) {
-		
+		android.setSoftware(this);
+
 	}
 	
 	
 	public void vonKaempferBenutzt(G_Kaempfer android) {
-		
+		android.setSoftware(null);
+
 	}
 }
