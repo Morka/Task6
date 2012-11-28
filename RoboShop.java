dfc83d3315404e1b1eba77cd8161fd428a809086
Morka <Nief@mx.at>
2012-11-28 10:11:09 +0100

    assertions and comments

--- RoboShop.java
+++ RoboShop.java
@@ -16,9 +16,17 @@ public class RoboShop {
 		map = new HashMap<Integer,Android>();
 		stack = new Stack<Android>();
 	}
-	
-	
-	
+
+	/**
+	 * insert new Android into roboShop if its functional or changes
+	 * old android
+	 * 
+	 * @param android
+	 *
+	 */
+	//precondition: android != null
+	//postcondition: if new Android is functional it is on the stack and in the map
+	//				 otherwise it gives notice.
 	public void insert(Android android) {
 		Android delivery;
 		if((delivery = findAndroid(android.getSNumber())) == null){
@@ -32,16 +40,27 @@ public class RoboShop {
 				System.out.println("Fehler bei Androidauslieferung");
 		}
 		else{
+			stack.add(delivery);
 			delivery.changeAndroid(android.deliverAndroid());
 			delivery = delivery.deliverAndroid();
-			stack.add(delivery);
 		}
 	}
-	
+	/**
+	 * finds Android in map
+	 * @param snr
+	 * @return gives back Android with specified snr or null if not found
+	 */
 	private Android findAndroid(int snr){
 		return map.get(snr);
 	}
-	
+	/**
+	 * finds Android with given snr if existing otherwise error notice
+	 *
+	 * @param snr
+	 *Ê
+	 * @return string that describes the android or error notice
+	 */
+	//postcondition: descirptive string is given back if found, or an error notice
 	public String find(int snr) {
 		Android droid;
 		if((droid = map.get(snr)) != null)
